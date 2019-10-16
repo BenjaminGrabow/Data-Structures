dc8ae13e24a3f90e64a0338ebc4ad7f74b4ca75a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-16 14:00:00 +0200

    added solutions to insert and contains method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -6,26 +6,86 @@ from dll_stack import Stack
 
 class BinarySearchTree:
     def __init__(self, value):
+        # set the value at the current node
         self.value = value
+        # add ref to left child node
         self.left = None
+        # add ref to the right child node
         self.right = None
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # LEFT CASE
+        # check if the new nodes value is less than our current ones value
+        if value < self.value:
+            # if the is no left child,
+            if self.left != None: 
+                # place a new node here
+                self.left.insert(value)
+            # otherwise
+            else:
+                # repeat process for left
+                self.left = BinarySearchTree(value)
+        # RIGHT CASE
+        # check if the new nodes value is greater than or equal to the current parent value
+        elif value > self.value:
+            # if there is no right child here,
+            if self.right != None: 
+                # place a new one
+                self.right.insert(value)
+            # otherwise
+            else:
+                # repeat process right
+                self.right = BinarySearchTree(value)
+        
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # BASE CASE
+        if self.value == target:
+          return True
+        # LEFT CASE
+        if self.value > target:
+          if self.left == None:
+            return False
+          return self.left.contains(target)
+        # RIGHT CASE
+        elif self.value < target:
+          if self.right == None:
+            return False
+          return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # BASE CASE 
+        # if empty tree
+            # return none
+        
+        # RECURSIVE
+        # if the the is no right value
+            # return the root value
+        # return the get max of the the right node
+
+        # ITTERATIVE
+        # set a max value variable to keep track of max value
+        # get a ref to current node
+        # check if we are at a valid tree node
+            # if our current value is greater than the max value
+                # update the max value
+            # move on to the next right node in the tree
+            # setting the current node to the current nodes right
+        # return our max value
         pass
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
+        # base case
+
+        # left case
+
+        # right case
         pass
 
     # DAY 2 Project -----------------------
