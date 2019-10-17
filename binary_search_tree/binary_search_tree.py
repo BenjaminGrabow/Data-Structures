0a0c476f2f701d5e5b18032caee9863e9e320864
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-17 13:22:27 +0200

    added solutions to bft and dft

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -113,13 +113,41 @@ class BinarySearchTree:
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        queue = Queue()
         
+        queue.enqueue(node)
+
+        while queue.len():
+          current_node = queue.dequeue()
+
+          print(current_node.value)
+
+          if current_node.left:
+            queue.enqueue(current_node.left)
+
+          if current_node.right:
+            queue.enqueue(current_node.right)
+          
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        stack = Stack()
+
+        stack.push(node)
+
+        while stack.len():
+          current_node = stack.pop()
+
+          print(current_node.value)
+
+          
+          if current_node.left:
+            stack.push(current_node.left)
+
+          if current_node.right:
+            stack.push(current_node.right)
+
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
